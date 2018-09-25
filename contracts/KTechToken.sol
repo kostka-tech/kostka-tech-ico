83823dae1552143e9e3522cc0f6c55db528ff53d
Kostka Tech <kostka.tech@gmail.com>
2018-09-25 09:15:20 +0200

    Add support for token transfer

--- contracts/KTechToken.sol
+++ contracts/KTechToken.sol
@@ -8,8 +8,23 @@ contract KTechToken {
     uint256 public totalSupply;
     mapping(address => uint256) public balanceOf;
 
-    function KTechToken(uint256 _initialSupply)  public {
+    event Transfer(
+        address indexed _from,
+        address indexed _to,
+        uint256 _value
+    );
+
+    constructor(uint256 _initialSupply)  public {
         balanceOf[msg.sender] = _initialSupply;
         totalSupply = _initialSupply;
+    }
+
+    function transfer(address _to, uint256 _value) public returns(bool success) {
+        require(balanceOf[msg.sender] >= _value);
+        balanceOf[msg.sender] -= _value;
+        balanceOf[_to] += _value;
+        emit Transfer(msg.sender, _to, _value);
+
+        return true;
     }
 }
--- test/KTechToken.js
+++ test/KTechToken.js
@@ -31,4 +31,29 @@ contract('KTechToken', function(accounts) {
         });
     });
 
+    it('transfers tokens', function() {
+        return KTechToken.deployed().then(function(instance) {
+            tokenInstance = instance;
+            return tokenInstance.transfer.call(accounts[1], 999999999999999);
+        }).then(assert.fail).catch(function(error) {
+            assert(error.message.indexOf('revert') >= 0);
+            return tokenInstance.transfer.call(accounts[1], 250000, { from: accounts[0]});
+        }).then(function(success) {
+            assert.equal(success, true);
+            return tokenInstance.transfer(accounts[1], 250000, { from: accounts[0]});
+        }).then(function(receipt) {
+            assert.equal(receipt.logs.length, 1);
+            assert.equal(receipt.logs[0].event, 'Transfer');
+            assert.equal(receipt.logs[0].args._from, accounts[0]);
+            assert.equal(receipt.logs[0].args._to, accounts[1]);
+            assert.equal(receipt.logs[0].args._value, 250000);
+            return tokenInstance.balanceOf(accounts[1]);
+        }).then(function(balance) {
+            assert.equal(balance.toNumber(), 250000);
+            return tokenInstance.balanceOf(accounts[0]);
+        }).then(function(balance) {
+            assert.equal(balance.toNumber(), 41750000);
+        });
+    });
+
 });
