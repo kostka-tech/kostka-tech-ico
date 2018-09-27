61f6dfa874eaefddad802acdb4e113621c2d0546
Kostka Tech <kostka.tech@gmail.com>
2018-09-27 20:06:50 +0200

    Implemented buyTokens which allows people purchase tokens from the sale contract

--- contracts/KTechTokenSale.sol
+++ contracts/KTechTokenSale.sol
@@ -7,11 +7,29 @@ contract KTechTokenSale {
     address admin;
     KTechToken public tokenContract;
     uint256 public tokenPrice;
+    uint256 public tokensSold;
+
+    event Sell(address _buyer, uint256 _amount);
 
     constructor(KTechToken _tokenContract, uint256 _tokenPrice) public {
         admin = msg.sender;
         tokenContract = _tokenContract;
         tokenPrice = _tokenPrice;
+    }
+
+    // Taken from dapphub/ds-math
+    function multiply(uint x, uint y) internal pure returns (uint z) {
+        require(y == 0 || (z = x * y) / y == x);
+    }
+
+    function buyTokens(uint256 _numberOftokens) public payable {
+        require(msg.value == multiply(_numberOftokens, tokenPrice));
+        require(tokenContract.balanceOf(this) >= _numberOftokens);
+        require(tokenContract.transfer(msg.sender, _numberOftokens));
+
+        tokensSold += _numberOftokens;
+        
+        emit Sell(msg.sender, _numberOftokens);
     }
 
 }
--- test/KTechTokenSale.js
+++ test/KTechTokenSale.js
@@ -1,9 +1,15 @@
+var KTechToken = artifacts.require("./KTechToken.sol");
 var KTechTokenSale = artifacts.require("./KTechTokenSale.sol");
 
 contract('KTechTokenSale', function(accounts) {
 
+    var tokenInstance;
     var tokenSaleInstance;
     var tokenPrice = 1000000000000000; // wei (0.001 ether)
+    var tokensAvailable = 31500000;
+    var admin = accounts[0];
+    var buyer = accounts[1];
+    var numberOfTokens;
 
     it('init the contract with the correct values', function() {
         return KTechTokenSale.deployed().then(function(instance) {
@@ -17,6 +23,41 @@ contract('KTechTokenSale', function(accounts) {
             return tokenSaleInstance.tokenPrice();
         }).then(function(price) {
             assert.equal(price.toNumber(), tokenPrice);
+        });
+    });
+
+    it('allows users to purchase tokens', function() {
+        return KTechToken.deployed().then(function(instance) {
+            tokenInstance = instance;
+            return KTechTokenSale.deployed();
+        }).then(function(instance) {
+            tokenSaleInstance = instance;
+            return tokenInstance.transfer(tokenSaleInstance.address, tokensAvailable, { from: admin });
+        }).then(function(receipt) {
+            numberOfTokens = 10;
+            var value = numberOfTokens * tokenPrice;
+            return tokenSaleInstance.buyTokens(numberOfTokens, { from: buyer, value: value});
+        }).then(function(receipt) {
+            assert.equal(receipt.logs.length, 1);
+            assert.equal(receipt.logs[0].event, 'Sell');
+            assert.equal(receipt.logs[0].args._buyer, buyer);
+            assert.equal(receipt.logs[0].args._amount, numberOfTokens);
+            return tokenSaleInstance.tokensSold();
+        }).then(function(amount) {
+            assert.equal(amount.toNumber(), numberOfTokens);
+            return tokenInstance.balanceOf(buyer);
+        }).then(function(balance) {
+            assert.equal(balance.toNumber(), numberOfTokens);
+            return tokenInstance.balanceOf(tokenSaleInstance.address);
+        }).then(function(balance) {
+            assert.equal(balance.toNumber(), tokensAvailable - numberOfTokens);
+            return tokenSaleInstance.buyTokens(numberOfTokens, { from: buyer, value: 1});
+        }).then(assert.fail).catch(function(error) {
+            assert(error.message.indexOf('revert') >= 0);
+            var value = numberOfTokens * tokenPrice;
+            return tokenSaleInstance.buyTokens(35000000, { from: buyer, value: value});
+        }).then(assert.fail).catch(function(error) {
+            assert(error.message.indexOf('revert') >= 0);
         });
     });
 
