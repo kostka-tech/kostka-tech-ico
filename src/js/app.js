6e090555cb40a67f0d845ee0558c43b08a1fc736
Kostka Tech <kostka.tech@gmail.com>
2018-10-04 18:58:23 +0200

    Finish out the token sale website

--- src/js/app.js
+++ src/js/app.js
@@ -2,9 +2,12 @@ App = {
     web3Provider: null,
     contracts: {},
     account: '0x0',
+    loading: false,
+    tokenPrice: 1000000000000000,
+    tokensSold: 0,
+    tokensAvailable: 42000000,
 
     init: function() {
-        console.log('App initialized...');
         return App.initWeb3();
     },
 
@@ -36,19 +39,90 @@ App = {
                 App.contracts.KTechToken.deployed().then(function(ktechToken) {
                     console.log("Ktech Token Address:", ktechToken.address);
                 });
+                App.listenForEvents();
+                
                 return App.render();
             });
         })
     },
 
     render: function() {
+        if (App.loading) {
+            return;
+        }
+        App.loading = true;
+
+        var loader = $('#loader');
+        var content = $('#content');
+
+        loader.show();
+        content.hide();
+
         web3.eth.getCoinbase(function(err, account) {
             if(err === null) {
                 App.account = account;
                 $('#accountAddress').html("Your Account: " + account);
             }
         });
+
+        App.contracts.KTechTokenSale.deployed().then(function(instance) {
+            ktechTokenSaleInstance = instance;
+            return ktechTokenSaleInstance.tokenPrice();
+        }).then(function(tokenPrice) {
+            App.tokenPrice = tokenPrice.toNumber();
+            $('.token-price').html(web3.fromWei(App.tokenPrice, 'ether'));
+            return ktechTokenSaleInstance.tokensSold();
+        }).then(function(tokensSold) {
+            App.tokensSold = tokensSold.toNumber();
+            $('.tokens-sold').html(App.tokensSold);
+            $('.tokens-available').html(App.tokensAvailable);
+
+            var progressPercent = (Math.ceil(App.tokensSold) / App.tokensAvailable) * 100;
+            $('#progress').css('width', progressPercent + '%');
+
+            App.contracts.KTechToken.deployed().then(function(instance) {
+                ktechTokenInstance = instance;
+                return ktechTokenInstance.balanceOf(App.account);
+            }).then(function(balance) {
+                $('.ktt-balance').html(balance.toNumber());
+                App.loading = false;
+                loader.hide();
+                content.show();
+            })
+        });
+
+        App.loading = false;
+        loader.hide();
+        content.show();
     },
+
+    listenForEvents: function() {
+        App.contracts.KTechTokenSale.deployed().then(function(instance) {
+            instance.Sell({}, {
+                fromBlock: 0,
+                toBlock: 'latest',
+            }).watch(function(error, event) {
+                console.log("Sell event triggered", event);
+                App.render();
+            })
+        })
+    },
+
+    buyTokens: function() {
+        $('#content').hide();
+        $('#loader').show();
+        var numberOfTokens = $('#numberOfTokens').val();
+        App.contracts.KTechTokenSale.deployed().then(function(instance) {
+            return instance.buyTokens(numberOfTokens, {
+                from: App.account,
+                value: numberOfTokens * App.tokenPrice,
+                gas: 500000
+            });
+        }).then(function(result) {
+            console.log("Tokens bought...");
+            $('form').trigger('reset');
+        });
+    }
 };
 
 $(function() {
